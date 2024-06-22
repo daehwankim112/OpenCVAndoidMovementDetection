43d6f32874bd71c120ff90327545ad601cbbfb93
David Kim <46164193+daehwankim112@users.noreply.github.com>
2024-06-21 23:58:00 -0700

    chore: adding mediapipe

--- app/src/main/java/com/example/opencvprogramminghutmovementdetection/MainActivity.java
+++ app/src/main/java/com/example/opencvprogramminghutmovementdetection/MainActivity.java
@@ -15,8 +15,15 @@ import org.jetbrains.annotations.NotNull;
 import org.opencv.android.CameraActivity;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -24,6 +31,9 @@ import java.util.List;
 public class MainActivity extends CameraActivity {
     private static final String TAG = "OPENCV:APP";
     CameraBridgeViewBase cameraBridgeViewBase;
+    Mat curr_gray, prev_gray, rgb, diff;
+    List<MatOfPoint> cnts;
+    boolean is_init;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +42,17 @@ public class MainActivity extends CameraActivity {
 
         getPermission();
 
+        is_init = false;
+
         cameraBridgeViewBase = findViewById(R.id.cameraView);
         cameraBridgeViewBase.setCvCameraViewListener(new CameraBridgeViewBase.CvCameraViewListener2() {
             @Override
             public void onCameraViewStarted(int width, int height) {
-
+                curr_gray = new Mat();
+                prev_gray = new Mat();
+                rgb = new Mat();
+                diff = new Mat();
+                cnts = new ArrayList<>();
             }
 
             @Override
@@ -46,10 +62,35 @@ public class MainActivity extends CameraActivity {
 
             @Override
             public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-                return inputFrame.rgba();
-            }
-        });
+                if(!is_init) {
+                    prev_gray = inputFrame.gray();
+                    is_init = true;
+                    return prev_gray;
+                }
 
+                rgb = inputFrame.rgba();
+                curr_gray = inputFrame.gray();
+
+                //todo: detect noises
+                Core.absdiff(curr_gray, prev_gray, diff);
+                Imgproc.threshold(diff, diff, 40, 255, Imgproc.THRESH_BINARY);
+                Imgproc.findContours(diff, cnts, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
+                Imgproc.drawContours(rgb, cnts, -1, new Scalar(255, 0, 0), 5);
+
+                for (MatOfPoint m:cnts) {
+                    Rect r = Imgproc.boundingRect(m);
+                    Imgproc.rectangle(rgb, r, new Scalar(0, 255, 0), 4);
+                }
+
+                cnts.clear();
+                /////
+
+                prev_gray = curr_gray.clone();
+                return rgb;
+            }
+
+        });
         if (OpenCVLoader.initLocal()) {
             cameraBridgeViewBase.enableView();
             Log.d(TAG, "OpenCV initialization failed");
